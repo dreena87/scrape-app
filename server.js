247cf1726f7496381abfd6adf2be2e55f25c3aba
Adreena Howard <adreena.howard@yahoo.com>
2019-02-06 19:14:32 -0600

    added port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 
 // Set up our port to be either the host's designated port, or 3000
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || PORT;
 
 // Instantiate our Express App
 var app = express();
